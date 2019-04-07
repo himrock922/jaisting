8eb7e135915dc1ea9c7fd84751eadada05392842
User Himrock922 <himrock922@gmail.com>
2019-04-07 19:25:05 +0900

    テスト見直し

--- networks/tests.py
+++ networks/tests.py
@@ -1,7 +1,7 @@
 from django.test import TestCase
 
 
-class NetworksViewTest(TestCase):
+class TemplateTest(TestCase):
     def test_login_require_networks_new(self):
         response = self.client.get('/networks/new')
         self.assertRedirects(response, '/accounts/login/?next=/networks/new')
