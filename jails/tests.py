c6df79d6c0cc19c56ee5ade2b75f91395f754169
User Himrock922 <himrock922@gmail.com>
2019-04-07 19:36:50 +0900

    テスト見直し

--- jails/tests.py
+++ jails/tests.py
@@ -1,13 +1,16 @@
 from django.test import TestCase, Client
 from django.contrib.auth import get_user_model
 
+
 class TemplateTest(TestCase):
     def test_login_require_jails(self):
         response = self.client.get('/jails/')
         self.assertRedirects(response, '/accounts/login/?next=/jails/')
+
     def test_login_require_jail_new(self):
         response = self.client.get('/jails/new')
         self.assertRedirects(response, '/accounts/login/?next=/jails/new')
+
     def test_access_jails(self):
         User = get_user_model()
         self.client = Client()
@@ -15,6 +18,7 @@ class TemplateTest(TestCase):
         response = self.client.get('/jails/')
         self.assertTemplateUsed(response, 'jails/index.html')
         self.client.logout()
+
     def test_access_jail_new(self):
         User = get_user_model()
         self.client = Client()
@@ -22,4 +26,3 @@ class TemplateTest(TestCase):
         response = self.client.get('/jails/new')
         self.assertTemplateUsed(response, 'jails/new.html')
         self.client.logout()
-
